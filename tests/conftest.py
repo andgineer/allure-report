afb53f1c668816b8c292289d0a7052cf1f661867
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-04-13 09:22:41 +0200

    run tests in temp

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import os
 import pathlib
+import shutil
+import tempfile
 from unittest.mock import patch
 
 import pytest
@@ -30,19 +32,28 @@ def docker_compose_file():
 
 @pytest.fixture
 def env():
-    with patch.dict(os.environ, {
-        "GITHUB_REPOSITORY": "owner/repo",
-        "GITHUB_REPOSITORY_OWNER": "owner",
-        "GITHUB_SERVER_URL": "https://github.com",
-        "GITHUB_RUN_NUMBER": "1",
-        "GITHUB_RUN_ID": "1",
-        "INPUT_ALLURE-RESULTS": "tests/resources/allure-results",
-        "INPUT_WEBSITE-SOURCE": "tests/resources/website-source",
-        "INPUT_REPORT-PATH": "report-path",
-        "INPUT_ALLURE-REPORT": "tests/resources/temp/allure-report",
-        "INPUT_WEBSITE-URL": "",
-        "INPUT_MAX-REPORTS": "20",
-        "INPUT_CI-NAME": "GitHub Action: CI",
-        "INPUT_REPORT-NAME": "Allure Report",
-    }):
-        yield
+    # Create a temporary directory
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_path = pathlib.Path(temp_dir)
+        # Copy all contents of RESOURCES to the temporary directory
+        shutil.copytree(RESOURCES, temp_path / 'resources', dirs_exist_ok=True)
+
+        # Setup the environment variables to use paths in the temporary directory
+        env_vars = {
+            "GITHUB_REPOSITORY": "owner/repo",
+            "GITHUB_REPOSITORY_OWNER": "owner",
+            "GITHUB_SERVER_URL": "https://github.com",
+            "GITHUB_RUN_NUMBER": "1",
+            "GITHUB_RUN_ID": "1",
+            "INPUT_ALLURE-RESULTS": str(temp_path / 'resources/allure-results'),
+            "INPUT_WEBSITE-SOURCE": str(temp_path / 'resources/website-source'),
+            "INPUT_REPORT-PATH": "report-path",
+            "INPUT_ALLURE-REPORT": str(temp_path / 'resources/temp/allure-report'),
+            "INPUT_WEBSITE-URL": "",
+            "INPUT_MAX-REPORTS": "20",
+            "INPUT_CI-NAME": "GitHub Action: CI",
+            "INPUT_REPORT-NAME": "Allure Report",
+        }
+
+        with patch.dict(os.environ, env_vars):
+            yield
